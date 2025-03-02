c870288b3f97f1024d3b563de3fad113b67c2df3
Tanushree Chakravorty <tanushree998@gmail.com>
2025-03-02 09:26:24 +0530

    text size adjusted

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { BarChart2, Home, Menu } from "lucide-react";
+import { BarChart2, ChevronDown, Home, Menu } from "lucide-react";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
@@ -28,7 +28,7 @@ const Sidebar = () => {
       }`}
       animate={{ width: isSidebarOpen ? 220 : 64 }}
     >
-      <div className="h-full bg-white backdrop-blur-md py-6 px-3 flex flex-col border-r border-gray-200">
+      <div className="h-full bg-white backdrop-blur-md py-6 px-2 flex flex-col border-r border-gray-200">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
           className="p-2 cursor-pointer transition-colors max-w-fit"
@@ -36,7 +36,7 @@ const Sidebar = () => {
           {isSidebarOpen ? <img src="/icon.png" alt="" /> : <Menu size={20} />}
         </button>
 
-        <nav className="mt-6 md:mt-4 grow">
+        <nav className="mt-6 md:mt-4 grow px-2">
           {SIDEBAR_ITEMS.map((item, idx) => (
             <Link key={idx} to={item.href}>
               <motion.div className="flex items-center p-2 text-sm font-medium rounded-lg hover:bg-gray-100 transition-colors mb-2">
@@ -61,6 +61,24 @@ const Sidebar = () => {
             </Link>
           ))}
         </nav>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-0.5">
+            <span className="bg-green-700 p-2 rounded-full text-sm text-white">
+              WI
+            </span>
+            <a href="#">
+              <ChevronDown size={20} />
+            </a>
+          </div>
+          {isSidebarOpen && (
+            <div>
+              <p className="text-sm text-end text-gray-800">CMP1Y</p>
+              <span className="text-xs text-gray-500">
+                Valid till Apr 19, 2025
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </motion.div>
   );
